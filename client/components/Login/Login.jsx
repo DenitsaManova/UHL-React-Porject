4bca3e66155ac65568bc01539013f4fe2156518e
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-24 17:13:23 +0200

    Change login to controlled form

--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -1,20 +1,40 @@
+import useForm from '../../src/hooks/useForm'
+
 import styles from './Login.module.css'
 
-
 export default function Login() {
+    const { values, onChange, onSubmit } = useForm({
+        email: '',
+        password: '',
+    });
     return (
-        
- <div className={styles["form-container"]}>
-        <h2 className={styles["login-title"]}> Login </h2>
-        <input type="text" name="text" className={styles["userName"]} placeholder="username"/>
-        <input type="password" name="password" className={styles["userPassword"]} placeholder="password"/>
-        <button className={styles["loginBtn"]}> Login </button>
-        <p className={styles["message"]}>
-            Not registered? <a href="/register">Create an account</a>
-          </p>
-    </div>
-        
-       
+        <form onSubmit={onSubmit}>
+            <div className={styles["form-container"]}>
+                <h2 className={styles["login-title"]}> Login </h2>
+
+                <input type="text" 
+                name="text" 
+                className={styles["email"]} 
+                placeholder="example@abv.bg" 
+                onChange={onChange}
+                value={values.email}/>
+
+                <input type="password" 
+                name="password" 
+                className={styles["userPassword"]} 
+                placeholder="password" 
+                onChange={onChange}
+                value={values.password}/>
+
+                <button className={styles["loginBtn"]}> Login </button>
+
+                <p className={styles["message"]}>
+                    Not registered? <a href="/register">Create an account</a>
+                </p>
+            </div>
+        </form>
+
+
 
     )
 }
