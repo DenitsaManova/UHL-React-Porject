bf580360a5f4764cdd03095ba5c3282556f32ba8
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-24 17:14:12 +0200

    Update Register.jsx

--- client/components/Register/Register.jsx
+++ client/components/Register/Register.jsx
@@ -2,6 +2,7 @@ import styles from './Register.module.css'
 
 export default function Regsiter() {
     return (
+        <form>
         <div className={styles["form-container"]}>
         <h2 className={styles["register-title"]}> Register </h2>
         <input type="text" name="text" className={styles["userName"]} placeholder="username"/>
@@ -9,9 +10,11 @@ export default function Regsiter() {
         <input type="password" name="repeatPassword" className={styles["userPassword"]} placeholder="repeatPassword"/>
         <button className={styles["registerBtn"]}> Register </button>
         <p className={styles["message"]}>
-        Already registered? <a href="/login">Register</a>
+        Already registered? <a href="/login">Login</a>
           </p>
     </div>
+    </form>
+
 
     )
 }
