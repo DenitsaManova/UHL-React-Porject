efc5d65e6c49d2d1866045b981611b25e7b86f23
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-25 15:51:07 +0200

    Add auth service
    Add paths
    Add login redirect

---
+++ client/src/paths.js
@@ -0,0 +1,12 @@
+const Path = {
+    Home: '/',
+    Login: '/login',
+    Register: '/register',
+    Logout: '/logout',
+    Posts: '/posts',
+    PostDetails: '/posts/details/:postId',
+    CreatePost: '/posts/create',
+
+};
+
+export default Path;
