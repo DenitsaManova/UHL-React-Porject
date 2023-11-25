35de7977fcd97ab028be81ca5b1a2a240b69bf08
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-25 18:31:34 +0200

    Add optional navigation

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,31 +1,44 @@
-import styles from './Header.module.css'
-import logo from './uhl-logo.png'
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import AuthContext from '../../src/contexts/authContext';
+
+import styles from './Header.module.css'
+import logo from './uhl-logo.png'
+
 export default function Header() {
+    const { isAuthenticated, email } = useContext(AuthContext);
     return (
-        <header> 
+        <header>
             <nav className={styles.navbar}>
-            <div className={styles.left}>
-            <Link to="/">
+                <div className={styles.left}>
+                    <Link to="/">
                         <img
                             src={logo}
                             alt="UHL"
                             className={styles.logo}
                         />
                     </Link>
-            </div>
-            <div className={styles.right}>
-                <ul className={styles.list}>
-                <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/posts'>Posts</Link></li>
-                    <li><Link to='/posts/create'>Create Post</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
-                    <li><Link to='/register'>Register</Link></li>
-                    <li><Link to='/logout'>Logout</Link></li>
-                </ul>
-            </div>
-        </nav>
+                </div>
+                <div className={styles.right}>
+                    <ul className={styles.list}>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/posts'>Posts</Link></li>
+                        {isAuthenticated && (
+                            <>
+                                <li><Link to='/posts/create'>Create Post</Link></li>
+                                <li><Link to='/logout'>Logout</Link></li>
+                            </>
+                        )}
+                        {!isAuthenticated && (
+                            <>
+                                <li><Link to='/login'>Login</Link></li>
+                                <li><Link to='/register'>Register</Link></li>
+                            </>
+                        )}
+                    </ul>
+                </div>
+            </nav>
         </header>
 
     )
