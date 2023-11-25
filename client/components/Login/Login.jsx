d7ff815e6006fd2bcd96b39a508f0cc25b7a80e0
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-25 12:41:52 +0200

    Add login form keys

--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -2,29 +2,36 @@ import useForm from '../../src/hooks/useForm'
 
 import styles from './Login.module.css'
 
-export default function Login() {
-    const { values, onChange, onSubmit } = useForm({
-        email: '',
-        password: '',
+const  LoginFormKeys = {
+    Email: 'email',
+    Password: 'password',
+}
+
+export default function Login({
+    loginSubmitHandler,
+}) {
+    const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+        [LoginFormKeys.Email]: '',
+        [LoginFormKeys.Password]: '',
     });
     return (
         <form onSubmit={onSubmit}>
             <div className={styles["form-container"]}>
                 <h2 className={styles["login-title"]}> Login </h2>
 
-                <input type="text" 
-                name="text" 
-                className={styles["email"]} 
-                placeholder="example@abv.bg" 
-                onChange={onChange}
-                value={values.email}/>
+                <input type="email"
+                    name={LoginFormKeys.Email}
+                    className={styles["email"]}
+                    placeholder="example@abv.bg"
+                    onChange={onChange}
+                    value={values[LoginFormKeys.Email]} />
 
-                <input type="password" 
-                name="password" 
-                className={styles["userPassword"]} 
-                placeholder="password" 
-                onChange={onChange}
-                value={values.password}/>
+                <input type="password"
+                    name={LoginFormKeys.Password}
+                    className={styles["userPassword"]}
+                    placeholder="password"
+                    onChange={onChange}
+                    value={values[LoginFormKeys.Password]} />
 
                 <button className={styles["loginBtn"]}> Login </button>
 
