2ca509a179ef0a2711d6472706d81ca8617c7939
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-25 18:32:22 +0200

    Change register form to controlled

--- client/components/Register/Register.jsx
+++ client/components/Register/Register.jsx
@@ -1,19 +1,57 @@
+import { useContext } from 'react'
 import styles from './Register.module.css'
+import AuthContext from '../../src/contexts/authContext'
+import useForm from '../../src/hooks/useForm';
+
+const RegisterFormKeys = {
+    Email: 'email',
+    Password: 'password',
+    RepeatPassword: 'repeatPassword',
+}
 
 export default function Regsiter() {
+    const { registerSubmitHandler } = useContext(AuthContext);
+    const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+        [RegisterFormKeys.Email]: '',
+        [RegisterFormKeys.Password]: '',
+        [RegisterFormKeys.RepeatPassword]: '',
+    });
+
     return (
-        <form>
-        <div className={styles["form-container"]}>
-        <h2 className={styles["register-title"]}> Register </h2>
-        <input type="text" name="text" className={styles["userName"]} placeholder="username"/>
-        <input type="password" name="password" className={styles["userPassword"]} placeholder="password"/>
-        <input type="password" name="repeatPassword" className={styles["userPassword"]} placeholder="repeatPassword"/>
-        <button className={styles["registerBtn"]}> Register </button>
-        <p className={styles["message"]}>
-        Already registered? <a href="/login">Login</a>
-          </p>
-    </div>
-    </form>
+        <form onSubmit={onSubmit}>
+            <div className={styles["form-container"]}>
+                <h2 className={styles["register-title"]}> Register </h2>
+
+                <input
+                    type="email"
+                    name="email"
+                    className={styles["email"]}
+                    placeholder="example@abv.bg"
+                    onChange={onChange}
+                    values={values[RegisterFormKeys.Email]} />
+
+                <input
+                    type="password"
+                    name="password"
+                    className={styles["userPassword"]}
+                    placeholder="password"
+                    onChange={onChange}
+                    values={values[RegisterFormKeys.Password]} />
+
+                <input
+                    type="password"
+                    name="repeatPassword"
+                    className={styles["userPassword"]}
+                    placeholder="repeat password"
+                    onChange={onChange}
+                    values={values[RegisterFormKeys.RepeatPassword]} />
+
+                <button className={styles["registerBtn"]}> Register </button>
+                <p className={styles["message"]}>
+                    Already registered? <a href="/login">Login</a>
+                </p>
+            </div>
+        </form>
 
 
     )
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+import * as authService from './services/authService';
+import AuthContext from './contexts/authContext';
+import Path from './paths';
 
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer";
@@ -10,25 +15,46 @@ import Posts from '../components/Posts/Posts';
 import Details from '../components/Details/Details';
 
 
+
 function App() {
-  
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState({});
+
+  const loginSubmitHandler = async (values) => {
+   const result = await authService.login(values.email, values.password);
+   
+   setAuth(result);
+
+   navigate(Path.Home);
+  };
+
+  const registerSubmitHandler = async (values) => {
+    console.log(values);
+  }
+
+  const values = {
+    loginSubmitHandler,
+    registerSubmitHandler,
+    email: auth.email,
+    isAuthenticated: !!auth.email,
+  }
+
   return (
-    <>
-    <Header />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    {/* TODO: Add route to details and edit page */}
-    <Route path="/posts" element={<Posts />} />
-    <Route path="/posts/details/:postId" element={<Details />} />
-    <Route path="/posts/create" element={<Create />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Regsiter />} />
-
-    </Routes>
-    <Footer />
-
-
-    </>
+    <AuthContext.Provider value={values}>
+      <div>
+        <Header />
+        <Routes>
+          <Route path={Path.Home} element={<Home />} />
+          {/* TODO: Add route for edit page */}
+          <Route path={Path.Posts} element={<Posts />} />
+          <Route path={Path.PostDetails} element={<Details />} />
+          <Route path={Path.CreatePost} element={<Create />} />
+          <Route path={Path.Login} element={<Login />} />
+          <Route path={Path.Register} element={<Regsiter />} />
+        </Routes>
+        <Footer />
+      </div>
+    </AuthContext.Provider>
   )
 }
 
