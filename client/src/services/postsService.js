87a290d9cb1e5a6c11ba2e8dea044bbc5824acc9
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-27 21:07:28 +0200

    Migrate to collections from jsonstore

--- client/src/services/postsService.js
+++ client/src/services/postsService.js
@@ -1,11 +1,11 @@
 import * as request  from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/jsonstore/posts';
+const baseUrl = 'http://localhost:3030/data/posts';
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
     
-    return Object.values(result);
+    return result;
 }
 
 export const getOne = async (postId) => {
