b351821ac682f04cf1b8c348a9bb77c244959a84
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-28 22:40:45 +0200

    Update authContext

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,4 +1,10 @@
 import { createContext } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+import * as authService from '../services/authService';
+import Path from '../paths';
+import usePersistedState from "../hooks/usePersistedState";
 
 const AuthContext = createContext();
 
@@ -6,13 +12,46 @@ AuthContext.displayName = 'AuthContext';
 
 export const AuthProvider = ({
     children,
-    value
 }) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = usePersistedState('auth', {});
+  
+    const loginSubmitHandler = async (values) => {
+      const result = await authService.login(values.email, values.password);
+  
+      setAuth(result);
+      localStorage.setItem('accessToken', result.accessToken);
+      navigate(Path.Home);
+    };
+  
+    const registerSubmitHandler = async (values) => {
+      const result = await authService.register(values.email, values.password);
+  
+      setAuth(result);
+      localStorage.setItem('accessToken', result.accessToken);
+      navigate(Path.Home);
+    };
+  
+    const logoutHandler = () => {
+      setAuth({});
+      localStorage.removeItem('accessToken');
+      navigate(Path.Home);
+  
+    };
+  
+    const values = {
+      loginSubmitHandler,
+      registerSubmitHandler,
+      logoutHandler,
+      email: auth.email,
+      isAuthenticated: !!auth.email,
+    };
+
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={values}>
             {children}
         </AuthContext.Provider>
     );
 };
-
+ 
  export default AuthContext;
