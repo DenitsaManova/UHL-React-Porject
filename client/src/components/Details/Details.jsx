d19a00e63127ce6b94ec12019f1ce58f1b0304c3
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-11-30 18:56:16 +0200

    Show owner buttons and like button for not owner

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,11 +1,13 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import * as postService from '../../services/postsService';
 
 import styles from './Details.module.css'
+import AuthContext from "../../contexts/authContext";
 
 export default function Details() {
+    const { isAuthenticated, email, userId } = useContext(AuthContext);
     const [post, setPost] = useState({});
     const { postId } = useParams();
 
@@ -14,26 +16,42 @@ export default function Details() {
             .then(setPost);
     }, [postId]);
 
-    return (
-         <section className={styles["container"]}>
-        <div className={styles["blog-item"]}>
-            <div className={styles["blog-image"]}>
-            <img src={post.imageUrl} alt={post.title}/>
-            </div>
-            <h4>{post.title}</h4>
-            <div className={styles["description"]}>
-            <p>{post.description}</p>
-            </div>
-            {/* <div className={styles["blog-details"]}> */}
-            <div>
-                
-                 <button className={styles["editBtn"]}> Edit </button> 
-                 <button className={styles["deleteBtn"]}> Delete </button> 
-                 {/* <Link to={`/posts/`} className={styles["detailsBtn"]}>Details</Link>  */}
-                
+    const isOwner = userId === post._ownerId;
+    //console.log(useContext(AuthContext));
+    //console.log(email);
+    //console.log(post._ownerId);
 
+    return (
+        <section className={styles["container"]}>
+            <div className={styles["blog-item"]}>
+                <div className={styles["blog-image"]}>
+                    <img src={post.imageUrl} alt={post.title} />
+                </div>
+                <h4>{post.title}</h4>
+                <div className={styles["description"]}>
+                    <p>{post.description}</p>
+                </div>
+                {/* <div className={styles["blog-details"]}> */}
+                <div>
+                    {isAuthenticated && (
+                        <div>
+                            {isOwner && (
+                                <div>
+                                    <button className={styles["editBtn"]}> Edit </button>
+                                    <button className={styles["deleteBtn"]}> Delete </button>
+                                    {/* <Link to={`/posts/`} className={styles["detailsBtn"]}>Details</Link>  */}
+                                </div>
+                            )}
+                            {!isOwner && (
+                                <div>
+                                    <button className={styles["editBtn"]}> Like </button>
+                                    <span>0</span>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
-         </section>
+        </section>
     )
 }
--- client/src/services/postsService.js
+++ client/src/services/postsService.js
@@ -1,6 +1,6 @@
 import * as request  from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/data/posts';
+const baseUrl = ' http://localhost:3030/data/posts';
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
