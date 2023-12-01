dadd2953892e1350f134355f2d7e97b8d3501a6f
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-12-01 14:52:51 +0200

    Add edit functionality

--- client/src/components/Edit/Edit.jsx
+++ client/src/components/Edit/Edit.jsx
@@ -1,14 +1,44 @@
-import styles from './Edit.module.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import * as postService from '../../services/postsService';
 
-export default function Edit(){
+import styles from './Edit.module.css';
+import useForm from '../../hooks/useForm';
+import { useEffect, useState } from 'react';
+
+export default function Edit() {
+    const navigate = useNavigate();
+    const { postId } = useParams();
+    const [post, setPost] = useState({
+        title: '',
+        imageUrl: '',
+        descrioption: '',
+    });
+
+    useEffect(() => {
+        postService.getOne(postId)
+            .then(result => {
+                setPost(result);
+            });
+    }, [postId]);
+
+    const editPostSubmitHandler = async (values) => {
+        try {
+            await postService.edit(postId, values);
+            navigate('/posts');
+        } catch (error) {
+            //add error notification
+            console.log(error);
+        }
+    }
+    const { values, onChange, onSubmit } = useForm(editPostSubmitHandler, post);
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className={styles["form-container"]}>
                 <h2 className={styles["edit-title"]}> Edit Post </h2>
-                <input type="text" name="title" className={styles["edit"]} placeholder="Title..." />
-                <input type="text" name="imageUrl" className={styles["edit"]} placeholder="https://imgUrl..." />
-                <input type="text" name="description" className={styles["edit"]} placeholder="Description..." />
-                <button className={styles["editBtn"]}> Create </button>
+                <input type="text" name="title" className={styles["edit"]} value={values.title} onChange={onChange} placeholder="Title..." />
+                <input type="text" name="imageUrl" className={styles["edit"]} value={values.imageUrl} onChange={onChange} placeholder="https://imgUrl..." />
+                <input type="text" name="description" className={styles["edit"]} value={values.description} onChange={onChange} placeholder="Description..." />
+                <button className={styles["editBtn"]}> Edit </button>
             </div>
         </form>
 
