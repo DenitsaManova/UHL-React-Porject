e87ee1618103dde7a008cb29714bfa020e139eb8
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-12-08 11:02:03 +0200

    Add error handling for login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,49 +1,64 @@
-import { useContext } from 'react';
-import useForm from '../../hooks/useForm'
-
-import styles from './Login.module.css'
-import AuthContext from '../../contexts/authContext';
+import { useContext, useState } from "react";
+import useForm from "../../hooks/useForm";
+import styles from "./Login.module.css";
+import AuthContext from "../../contexts/authContext";
 
 const LoginFormKeys = {
-    Email: 'email',
-    Password: 'password',
-}
+  Email: "email",
+  Password: "password",
+};
 
 export default function Login() {
+  const { loginSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(handleSubmit, {
+    [LoginFormKeys.Email]: "",
+    [LoginFormKeys.Password]: "",
+  });
 
-    const { loginSubmitHandler } = useContext(AuthContext);
-    const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
-        [LoginFormKeys.Email]: '',
-        [LoginFormKeys.Password]: '',
-    });
-    return (
-        <form onSubmit={onSubmit}>
-            <div className={styles["form-container"]}>
-                <h2 className={styles["login-title"]}> Login </h2>
+  const [error, setError] = useState("");
 
-                <input type="email"
-                    name={LoginFormKeys.Email}
-                    className={styles["email"]}
-                    placeholder="example@abv.bg"
-                    onChange={onChange}
-                    value={values[LoginFormKeys.Email]} />
+  function handleSubmit() {
+    if (!values[LoginFormKeys.Email] || !values[LoginFormKeys.Password]) {
+      setError("Please fill in all fields!");
+      return;
+    }
 
-                <input type="password"
-                    name={LoginFormKeys.Password}
-                    className={styles["userPassword"]}
-                    placeholder="password"
-                    onChange={onChange}
-                    value={values[LoginFormKeys.Password]} />
+    setError("");
 
-                <button className={styles["loginBtn"]}> Login </button>
+    loginSubmitHandler(values);
+  }
 
-                <p className={styles["message"]}>
-                    Not registered? <a href="/register">Create an account</a>
-                </p>
-            </div>
-        </form>
+  return (
+    <form onSubmit={onSubmit}>
+      <div className={styles["form-container"]}>
+        <h2 className={styles["login-title"]}> Login </h2>
 
+        <input
+          type="email"
+          name={LoginFormKeys.Email}
+          className={styles["email"]}
+          placeholder="example@abv.bg"
+          onChange={onChange}
+          value={values[LoginFormKeys.Email]}
+        />
 
+        <input
+          type="password"
+          name={LoginFormKeys.Password}
+          className={styles["userPassword"]}
+          placeholder="password"
+          onChange={onChange}
+          value={values[LoginFormKeys.Password]}
+        />
 
-    )
+        {error && <p className={styles["error-message"]}>{error}</p>}
+
+        <button type="submit" className={styles["loginBtn"]}> Login </button>
+
+        <p className={styles["message"]}>
+          Not registered? <a href="/register">Create an account</a>
+        </p>
+      </div>
+    </form>
+  );
 }
